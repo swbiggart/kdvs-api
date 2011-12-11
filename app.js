d1e98dc0b9e993430c34f565b4f8b4bb29edf07a
Scott Woolsey Biggart <swbiggart@gmail.com>
2011-12-10 19:54:05 -0800

    Added basic playlist api. Needs more show info and should be optimized with iterators

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var jquery_url = 'http://code.jquery.com/jquery-1.5.min.js'
 
-var request = require('request'),
-    jsdom = require('jsdom');
 
-var express = require('express'),
+var request = require('request'),
+    jsdom = require('jsdom')
+    express = require('express'),
+    _und = require('underscore'),
     app = express.createServer();
     
 var KDVS = {
@@ -61,28 +62,81 @@ var KDVS = {
     }
   },
   schedule: {
+    get: function(req, res){
+      KDVS.schedule.request(req, res, KDVS.schedule.response);
+    },
+    request: function(req, res, callback){
+      var uri = KDVS.library_url + '/ajax/streamingScheduleJSON';
+      KDVS.request(uri, req, res, callback);      
+    },
     response: function(req, res, body){
       res.send(body); 
+    }
+  },
+  show: {
+    get: function(req, res){
+      KDVS.show.request(req, res, KDVS.show.response);
+    },
+    request: function(req, res, callback){
+      KDVS.schedule.request(req, res, callback);
+    },
+    response: function(req, res, body){
+      var schedule = JSON.parse(body);
+      var show = _und.find(schedule, function(show, key){
+        return show.show_id == req.params.id;
+      });
+      res.send(JSON.stringify(show));
+    }
+  },
+  playlist: {
+    response: function(req, res, body){
+      KDVS.jsdom(req, res, body, function(req, res, err, window ){
+        res.send(JSON.stringify(KDVS.playlist.extract(window))); 
+      });
     },
     get: function(req, res){
-      KDVS.schedule.request(req, res);
+      KDVS.playlist.request(req, res);
     },
     request: function(req, res){
-      var uri = KDVS.library_url + '/ajax/streamingScheduleJSON';
-      
-      KDVS.request(uri, req, res, KDVS.schedule.response);      
+      var uri = KDVS.url + '/show-info/' + req.params.show_id + '?date=' + req.params.date;
+      KDVS.request(uri, req, res, KDVS.playlist.response);      
     },
-  }
-  
+    extract: function(window){
+      var $ = window.jQuery;
+      var playlist = new Array();
+      var tracks = $('table tr:has(td)');
+      //replace this with a nice _und map function
+      tracks.each(function(track_num){
+        row = $('td', this);
+        if(row.size() == 1){ //airbreak
+          playlist[track_num] = {airbreak: true};
+        } else { //track
+          //this could be done with a nice _und function too, mapping key => index
+          playlist[track_num] = {
+            track: row.eq(0).text().trim(),
+            artist: row.eq(1).text().trim(),
+            song: row.eq(2).text().trim(),
+            album: row.eq(3).text().trim(),
+            label: row.eq(4).text().trim(),
+            comments: row.eq(5).text().trim(),
+            airbreak: false
+          };
+        }
+      });
+      return playlist;
+    }
+  },
 }
 
 
 app.use(express.logger());
 app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
 
-app.get('/schedule/', KDVS.schedule.get);
-app.get('/', KDVS.news.get);
 
+app.get('/', KDVS.news.get);
+app.get('/schedule', KDVS.schedule.get);
+app.get('/show/:id', KDVS.show.get);
+app.get('/show/:show_id/:date', KDVS.playlist.get);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
