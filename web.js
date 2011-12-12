36281fa5996a3a4d6ac92cf274cc08a8e808d0ad
Scott Woolsey Biggart <swbiggart@gmail.com>
2011-12-12 00:15:55 -0800

    Removed even more callbacks that were not needed

--- web.js
+++ web.js
@@ -6,72 +6,37 @@ var request = require('request'),
     express = require('express'),
     _und = require('underscore'),
     app = express.createServer();
-  
 
-//working!
 scrAPI = {
   url: 'http://kdvs.org',
   library_url: 'http://library.kdvs.org',
 
-  
-  request_http: function(uri, callback){
-    console.log('requesting ' + uri);
-    request({ uri: uri  }, function(error, response, body){
-      scrAPI.request_callback(error, response, body, callback);
-    });
-  },
-  
-  request_callback: function (error, response, body, callback) {
-    console.log('request_callback:');
-    if (error) {
-      if( response.statusCode !== 200){
-        console.log('Error when contacting ' + uri);
-      } else {
-        console.log('request error: ' + error);
-      }
-    };
-    callback(body);
-  },
-  
-  extract: function(window){
-    console.log('extract:');
-    var $ = window.jQuery;
-    var news = new Array();
-    var news_dom = $('#content-content > div').children('div.view-content');
-    
-    //maybe do this with an _und.map iterator
-    news_dom.children().each(function(index){
-      news[index] = {
-        "title": $(this).find('h2.title').text(),
-        "content": $(this).find('div.content').text()
-      };    
-    });
-    return news;
-  },
-  
-  response: function(body,callback){
-    console.log('response: ');
-    jsdom.env({
-      html: body,
-      scripts: [jquery_url],
-      done: function(err, window){
-        console.log('jsdom done');
-        callback(window); 
-      }
-    });
-  },
-  /*
-  get: function(req, res){
-    console.log('getting');
-    var uri = scrAPI.url + '/';
-    scrAPI.request_http(uri, function(body){
-      scrAPI.response(body, res, scrAPI.extract);
-    });
-  },*/
   get: function(uri, callback){
     console.log('getting');
-    scrAPI.request_http(uri, function(body){
-      scrAPI.response(body, callback);
+    scrAPI.request(uri, function(body){
+      console.log('response: ');
+      jsdom.env({
+        html: body,
+        scripts: [jquery_url],
+        done: function(err, window){
+          console.log('jsdom done');
+          callback(window); 
+        }
+      });
+    });
+  },
+  request: function(uri, callback){
+    console.log('requesting ' + uri);
+    request({ uri: uri  }, function(error, response, body){
+      console.log('request_callback:');
+      if (error) {
+        if( response.statusCode !== 200){
+          console.log('Error when contacting ' + uri);
+        } else {
+          console.log('request error: ' + error);
+        }
+      };
+      callback(body);
     });
   }
 };
