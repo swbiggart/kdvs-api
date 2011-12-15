84243b6bce0c00a672a6c0b5d5535ac54dd861b1
Scott Woolsey Biggart <swbiggart@gmail.com>
2011-12-14 20:20:02 -0500

    fully abstracted (maybe too much) memcache and all APIs using it (each with only a 60s lifetime) - first load takes ~2s, memcache ~6ms!

--- web.js
+++ web.js
@@ -16,7 +16,6 @@ memclient.connect();
 //function to grab the content of a page and return either a DOM to parse
 //or the raw content (in the case of JSON, XML, etc)
 function scrAPI(uri, callback, raw){
-
   //if raw paramater passed, and true, callback on raw body
   raw = typeof raw != 'undefined' ? raw : false;
 
@@ -43,7 +42,9 @@ function scrAPI(uri, callback, raw){
   });
 }
 
-function respond(req, res, uri, parser, lifetime){
+function respond(req, res, uri, parser, lifetime, raw){
+  raw = typeof raw != 'undefined' ? raw : false;
+  
   memclient.get(uri, function(error, result){
     if(error){
       console.log('memcache: error = ' + error);
@@ -53,12 +54,12 @@ function respond(req, res, uri, parser, lifetime){
       res.send(result);
     } else { //not in memchace
       scrAPI(uri, function(window){
-        var json = JSON.stringify(parser(window));
+        var json = parser(window);
         memclient.set(uri, json, function(error, result){
           console.log('memcache: stored '+ uri);
           res.send(json);
         }, lifetime);
-      });
+      }, raw);
     }
   });
 }
@@ -67,34 +68,11 @@ function respond(req, res, uri, parser, lifetime){
 var KDVS = {
   url: 'http://kdvs.org',
   library_url: 'http://library.kdvs.org',
-
-  //function isn't used, was just the first test I made
-  new_news: function(req, res){
-    var uri = KDVS.url + '/';
-    respond(req, res, uri, KDVS.news.parser, 60);
-  },
   
   news: {
     get:  function(req, res){
       var uri = KDVS.url + '/';
-      var lifetime = 60; //how long before scraping reset
-      memclient.get(uri, function(error, result){
-        if(error){
-          console.log('memcache: error = ' + error);
-        }
-        if(result && result != 'NOT_STORED'){ //found in memcache
-          console.log('memcache: found '+ uri);
-          res.send(result);
-        } else { //not in memchace
-          scrAPI(uri, function(window){
-            var json = JSON.stringify(KDVS.news.parser(window));
-            memclient.set(uri, json, function(error, result){
-              console.log('memcache: stored '+ uri);
-              res.send(json);
-            }, lifetime);
-          });
-        }
-      });
+      respond(req, res, uri, KDVS.news.parser, 60);
     },
     parser: function(window){
       var $ = window.jQuery;
@@ -106,28 +84,39 @@ var KDVS = {
           "content": $(this).find('div.content').html()
         };    
       });
-      return news;
+      return JSON.stringify(news);
     }
   },
-  schedule: function(req, res){
-    var uri = KDVS.library_url + '/ajax/streamingScheduleJSON';
-    var raw = true;
-    scrAPI(uri, function(body){res.send(body);}, raw);
+  schedule: {
+    get: function(req, res){
+      var uri = KDVS.library_url + '/ajax/streamingScheduleJSON';
+      var raw = true; //do not convert response to DOM
+      respond(req, res, uri, KDVS.schedule.parser, 60, raw);
+    },
+    parser: function(window){
+      return window;
+    }  
   },
-  show: function(req, res){
-    var uri = KDVS.library_url + '/ajax/streamingScheduleJSON';
-    var raw = true;
-    scrAPI(uri, function(body){
+  show: {
+    get: function(req, res){
+      var uri = KDVS.library_url + '/ajax/streamingScheduleJSON';
+      var raw = true;
+      respond(req, res, uri, KDVS.schedule.parser, 60, raw);
+    },
+    parser: function(window){
       var schedule = JSON.parse(body);
       var show = _und.find(schedule, function(show, key){
         return show.show_id == req.params.show_id;
       });
-      res.send(JSON.stringify(show));
-    }, raw);
+      return JSON.stringify(show);
+    }  
   },
-  playlist: function(req, res){
-    var uri = KDVS.url + '/show-info/' + req.params.show_id + '?date=' + req.params.date;
-    scrAPI(uri, function(window){
+  playlist:{
+    get: function(req, res){
+      var uri = KDVS.url + '/show-info/' + req.params.show_id + '?date=' + req.params.date;
+      respond(req, res, uri, KDVS.playlist.parser, 60);
+    },
+    parser:function(window){
       var $ = window.jQuery;
       var show = {};
 
@@ -154,51 +143,49 @@ var KDVS = {
         }
       });
       show.playlist = playlist
-      res.send(JSON.stringify(show));
-    });
+      return JSON.stringify(show);
+    }
   },
-  future: function(req, res){
-	  var uri = KDVS.url + '/show-future/' + req.params.show_id;
-	  scrAPI(uri, function(window){
-	    res.send(JSON.stringify(KDVS.parse_history(window)));
-	  });
-  },
-  past: function(req, res){
-	  var uri = KDVS.url + '/show-history/' + req.params.show_id;
-	  scrAPI(uri, function(window){
-	    res.send(JSON.stringify(KDVS.parse_history(window)));
-	  });
-  },
-  parse_history: function(window){
-    var $ = window.jQuery;
-    var history = [];
-    var shows = $('table tr'); //grab all rows from the table (except header)
-    //replace this with a nice _und map function perhaps?
-    shows.each(function(){
-      var row = $('td', this);
-      var date_time = row.eq(0).text().split('@'); //only temporary until I start caching data about shows
-      history.push({
-        day: $.trim(date_time[0]),
-        time: $.trim(date_time[1]),
-        comments: row.eq(1).clone().find('h4').remove().end().html(), 
-        image_url: row.eq(2).children('img').attr('src')
+  timeline: {
+    get: {
+      future: function(req, res){
+    	  var uri = KDVS.url + '/show-future/' + req.params.show_id;
+        respond(req, res, uri, KDVS.timeline.parser, 60);
+      },
+      past: function(req, res){
+    	  var uri = KDVS.url + '/show-history/' + req.params.show_id;
+        respond(req, res, uri, KDVS.timeline.parser, 60);
+      },
+    },
+    parser: function(window){
+      var $ = window.jQuery;
+      var history = [];
+      var shows = $('table tr'); //grab all rows from the table (except header)
+      //replace this with a nice _und map function perhaps?
+      shows.each(function(){
+        var row = $('td', this);
+        var date_time = row.eq(0).text().split('@'); //only temporary until I start caching data about shows
+        history.push({
+          day: $.trim(date_time[0]),
+          time: $.trim(date_time[1]),
+          comments: row.eq(1).clone().find('h4').remove().end().html(), 
+          image_url: row.eq(2).children('img').attr('src')
+        });
       });
-    });
-    return history; 
+      return JSON.stringify(history); 
+    }
   }
 }
 
 app.use(express.logger());
 app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
 
-app.get('/news', KDVS.new_news);
-
 app.get('/', KDVS.news.get);
-app.get('/schedule', KDVS.schedule);
-app.get('/show/:show_id', KDVS.show);
-app.get('/playlist/:show_id/:date', KDVS.playlist);
-app.get('/past/:show_id', KDVS.past);
-app.get('/future/:show_id', KDVS.future);
+app.get('/schedule', KDVS.schedule.get);
+app.get('/show/:show_id', KDVS.show.get);
+app.get('/playlist/:show_id/:date', KDVS.playlist.get);
+app.get('/past/:show_id', KDVS.timeline.get.past);
+app.get('/future/:show_id', KDVS.timeline.get.future);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
